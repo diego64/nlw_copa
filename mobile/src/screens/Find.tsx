387bda7a66735c289955a131294deb2e523e42a8
diego64 <diegoferreira1964@gmail.com>
2022-12-03 18:12:26 -0300

    Funcionalidade de encontrar um bolao

--- mobile/src/screens/Find.tsx
+++ mobile/src/screens/Find.tsx
@@ -1,10 +1,72 @@
-import { Heading, VStack } from "native-base";
+import { useState } from "react";
+import { Heading, useToast, VStack } from "native-base";
+import { useNavigation } from "@react-navigation/native";
 
 import { Header } from "../components/Header";
 import { Input } from "../components/Input";
 import { Button } from "../components/Button";
 
+import { api } from '../services/api';
+
 export function Find() {
+  const [isLoading, setIsLoding] = useState(false);
+  const [code, setCode] = useState('');
+
+  const toast = useToast();
+
+  const { navigate } = useNavigation();
+
+  async function handleJoinPool(){
+    try {
+      setIsLoding(true);
+
+      if(!code.trim()) {
+        return toast.show({
+          title: 'Informe o código',
+          placement: 'top',
+          bgColor: 'red.500'
+        });
+      }
+
+      await api.post('/pools/join', { code });
+
+      toast.show({
+        title: 'Voce entrou no bolão',
+        placement: 'top',
+        bgColor: 'green.500'
+      });
+
+      navigate('pools');
+
+    } catch (error) {
+      console.log(error);
+      setIsLoding(false);
+
+      if(error.response?.data?.message === 'Pool not Found.'){
+        return toast.show({
+          title: 'Boloão não encontrado!',
+          placement: 'top',
+          bgColor: 'red.500'
+        });
+      }
+
+      if(error.response?.data?.message === 'You already join this pool.'){
+        return toast.show({
+          title: 'Voce já está nesse bolão!',
+          placement: 'top',
+          bgColor: 'red.500'
+        });
+      }
+
+      toast.show({
+        title: 'Não foi possível encontrar o bolão',
+        placement: 'top',
+        bgColor: 'red.500'
+      });
+     
+    }
+  }
+
   return (
     <VStack flex={1} bgColor="gray.900">
       <Header title="Buscar por código" showBackButton/>
@@ -18,10 +80,14 @@ export function Find() {
         <Input
           mb={2}
           placeholder="Qual o código do bolão"
+          autoCapitalize="characters"
+          onChangeText={setCode}
         />
 
         <Button
           title="BUSCAR BOLÂO"
+          isLoading={isLoading}
+          onPress={handleJoinPool}
         />
       </VStack>
     </VStack>
